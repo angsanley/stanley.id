2aafd4d36bb6694bf17104af06a725fb2f05af2c
Stanley <angstanley68@gmail.com>
2021-03-05 13:49:21 +0700

    feat(navbar): Create nav items

---
+++ components/NavItems.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nuxt-link to="/" exact active-class="active">
+      Home
+    </nuxt-link>
+    <nuxt-link to="/projects" active-class="active">
+      Projects
+    </nuxt-link>
+    <nuxt-link to="/resume" active-class="active">
+      Resume
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavItems'
+}
+</script>
+
+<style scoped>
+  .active {
+    @apply font-bold;
+  }
+</style>
--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="navbar-container">
-    <div class="content">
-      <div class="branding">
+    <div class="content" :class="{ 'content-bg' : scrollPosition >= 10 }">
+      <div class="nav-button lg:hidden" />
+
+      <div class="branding" :class="{ 'branding-top': scrollPosition < 10 }">
         <nuxt-link to="/">
           Stanley Ang
         </nuxt-link>
+      </div>
+
+      <div class="hidden lg:block">
+        <nav-items class="nav-items" />
+      </div>
+
+      <div class="nav-button lg:hidden">
+        a
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import NavItems from '~/components/NavItems'
 export default {
-  name: 'TheNavbar'
+  name: 'TheNavbar',
+  components: { NavItems },
+  data () {
+    return {
+      scrollPosition: null
+    }
+  },
+
+  mounted () {
+    window.addEventListener('scroll', this.updateScroll)
+  },
+
+  methods: {
+    updateScroll () {
+      this.scrollPosition = window.scrollY
+    }
+  }
+
 }
 </script>
 
@@ -22,15 +50,31 @@ export default {
   }
 
   .content {
-    @apply w-full max-w-7xl h-16 bg-white rounded-2xl shadow-md bg-opacity-80 flex items-center px-8 mx-4;
+    @apply w-full max-w-7xl h-16 bg-opacity-0 flex justify-between items-center mx-4 transition-all duration-300 rounded-2xl;
     backdrop-filter: blur(5px);
   }
 
+  .content-bg {
+    @apply bg-white bg-opacity-80 shadow-md px-8;
+  }
+
   .branding {
-    @apply font-bold font-display text-xl transition duration-500;
+    @apply font-bold font-display text-xl transition-all duration-500;
+  }
+
+  .branding-top {
+    @apply text-2xl;
   }
 
   .branding:hover {
     @apply text-green-600;
+  }
+
+  .nav-button {
+    @apply w-8 h-8;
+  }
+
+  .nav-items {
+    @apply flex space-x-6;
   }
 </style>
