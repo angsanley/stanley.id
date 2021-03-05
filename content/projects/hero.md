34ad76f1679901a32fd47f6b5aab4a4c37267348
Stanley <angstanley68@gmail.com>
2021-03-05 22:03:33 +0700

    feat: Create projects page

---
+++ content/projects/hero.md
@@ -0,0 +1,6 @@
+---
+title: Projects
+description: Banyak kali bang, gabisa lagi dihitung.
+---
+
+Mau yang mana?
---
+++ pages/Projects.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <hero :reversed="false">
+      <template #left>
+        <h1>{{ heroContent.title }}</h1>
+      </template>
+
+      <template #right>
+        {{ heroContent.description }}
+      </template>
+    </hero>
+
+    <div class="container mx-auto">
+      <nuxt-content :document="heroContent" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Hero from '~/components/Hero'
+export default {
+  name: 'Projects',
+  components: { Hero },
+  async asyncData ({ $content }) {
+    const heroContent = await $content('projects/hero').fetch()
+
+    return {
+      heroContent
+    }
+  },
+  head () {
+    return {
+      title: `${this.heroContent.title} - Stanley Ang`,
+      meta: [
+        {
+          content: this.heroContent.description
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
