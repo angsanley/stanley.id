fa6e71365e874922cd9a7c718c29ce1869650fca
Stanley <angstanley68@gmail.com>
2021-07-24 03:27:41 +0700

    feat: project icons

--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,32 @@
         </hyperlink> studying Mobile Application and Technology.
         I enjoy developing modern 📱 mobile and 🌎 web apps.
       </p>
+
+      <p class="introduction">
+        Here’s some of the things that I worked on:
+      </p>
+
+      <div class="things">
+        <project-icon :icon-src="require('@/assets/icons/adab.png')" title="ADAB" description="Ayo Dengar Ayo Bicara" />
+        <project-icon :icon-src="require('@/assets/icons/rokaru.png')" title="Rokaru" description="Digital platform for MSME" />
+        <project-icon :icon-src="require('@/assets/icons/assignoti.png')" title="Assignoti" description="Tasks and assignment management" />
+        <project-icon :icon-src="require('@/assets/icons/tamago2.png')" title="Tamago" description="Digital piggy bank" />
+      </div>
+
+      <div>
+        <Hyperlink to="/projects">
+          ➡️ See more
+        </Hyperlink>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Hyperlink from '~/components/Hyperlink'
+import ProjectIcon from '~/components/ProjectIcon'
 export default {
-  components: { Hyperlink },
+  components: { ProjectIcon, Hyperlink },
   async asyncData ({ $content }) {
     const heroContent = await $content('index/hero').fetch()
     const whatIDoContent = await $content('index/what-i-do').fetch()
@@ -42,10 +60,26 @@ export default {
 
 <style scoped>
   .hero {
-    @apply pt-20 space-y-4;
+    @apply pt-20 space-y-8;
   }
 
   .introduction {
     @apply text-xl max-w-2xl;
+  }
+
+  .things {
+    @apply grid grid-flow-col grid-cols-2 grid-rows-2 gap-16 pt-4;
+  }
+
+  @screen md {
+    .things {
+      @apply grid-cols-4 grid-rows-1 gap-24;
+    }
+  }
+
+  @screen lg {
+    .things {
+      @apply grid-cols-4 grid-rows-1 gap-32;
+    }
   }
 </style>
