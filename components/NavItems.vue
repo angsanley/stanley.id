7be01654e0413b39abf4d97a5fa3917b373fa0d7
Stanley <angstanley68@gmail.com>
2021-07-24 04:07:02 +0700

    style(navitems): Hover color

--- components/NavItems.vue
+++ components/NavItems.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <nuxt-link to="/" exact active-class="active">
+    <nuxt-link to="/" exact active-class="active" class="link">
       🏠 Home
     </nuxt-link>
-    <nuxt-link to="/projects" active-class="active">
+    <nuxt-link to="/projects" active-class="active" class="link">
       Projects
     </nuxt-link>
-    <nuxt-link to="/resume" active-class="active">
+    <nuxt-link to="/resume" active-class="active" class="link">
       Resume
     </nuxt-link>
   </div>
@@ -19,4 +19,15 @@ export default {
 </script>
 
 <style scoped>
+  .active {
+    @apply text-green font-bold;
+  }
+
+  .link {
+    @apply transition duration-300 ease-in-out;
+  }
+
+  .link:hover {
+    @apply text-orange-light;
+  }
 </style>
