e08effd48e8e2f90ddc68beef73460753abe2dd2
Stanley <angstanley68@gmail.com>
2022-10-15 00:00:12 +0700

    refactor: change favicon and icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,10 +30,16 @@ export default {
     meta: [
       ...meta,
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'theme-color', content: '#8acb88' },
+      { name: 'msapplication-TileColor', content: '#8acb88' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/site.webmanifest' }
     ]
   },
 
