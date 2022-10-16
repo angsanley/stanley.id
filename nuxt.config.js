a42e45beb90a60456ecbbdfd1ce55a3c994a5000
Stanley <angstanley68@gmail.com>
2022-10-16 11:04:35 +0700

    refactor: change `theme_color`

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
       ...meta,
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'theme-color', content: '#8acb88' },
+      { name: 'theme-color', content: '#6CBC8A' },
       { name: 'msapplication-TileColor', content: '#8acb88' }
     ],
     link: [
