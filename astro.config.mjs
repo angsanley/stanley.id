7de8c7a1710949ecdd3b54066da5b48751f3b8f1
Stanley <angstanley68@gmail.com>
2024-07-19 16:40:29 +0700

    feat: seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import tailwind from '@astrojs/tailwind'
+import sitemapPlugin from '@astrojs/sitemap'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), icon()]
-});
+  integrations: [vue(), tailwind(), icon(), sitemapPlugin()],
+})
