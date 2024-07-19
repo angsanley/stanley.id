7bd879ab0ca4a273fdf0c79169dc55add91b97a7
Stanley <angstanley68@gmail.com>
2024-07-19 16:24:44 +0700

    feat: initial content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), tailwind(), icon()]
+});
